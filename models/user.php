e191cc5608999a5fdcd99aa813a8e600309ed772
Siclary Arreola <programador01@drg.mx>
2024-10-15 16:34:51 -0600

    espacio

--- models/user.php
+++ models/user.php
@@ -1,19 +1,25 @@
 <?php
 //datos para hacer la conexión a la base de datos
-class User {
+class User 
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+     {
         // Aquí se puede inicializar la conexión a la base de datos.
         $this->db = new Database();
     }
 
     // Método para autenticar al usuario
-    public function authenticate($email, $password) {
+    public function authenticate($email, $password) 
+    {
         // Aquí se debería realizar la verificación en la base de datos
-        if ($email == "test@example.com" && $password == "123456") {
+        if ($email == "test@example.com" && $password == "123456")
+         {
             return true;
-        } else {
+        } 
+        else 
+        {
             return false;
         }
     }
