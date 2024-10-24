83dfa54c7132fc6f6989372949a25a1b7a6543ec
Siclary Arreola <programador01@drg.mx>
2024-10-24 16:24:26 -0600

    archivo de directorio de rutas añadido

--- config/config.php
+++ config/config.php
@@ -12,3 +12,32 @@ define('DB_USER', 'root');
 
 define('DB_PASSWORD', '');
 
+
+// Ruta base del proyecto
+define('RUTA_BASE', __DIR__ . '/');  // Directorio raíz del proyecto
+
+// Rutas absolutas a vistas (views)
+define('ruta_login', RUTA_BASE . 'views/user/login.php');
+define('ruta_register', RUTA_BASE . 'views/user/register.php');
+define('ruta_forgot_password', RUTA_BASE . 'views/user/forgotPass.php');
+define('ruta_reset_password', RUTA_BASE . 'views/user/resetPass.php');
+define('ruta_verify_code', RUTA_BASE . 'views/user/verifyCode.php');
+define('ruta_dashboard_admin', RUTA_BASE . 'views/admin/dashboardAdmin.php');
+define('ruta_dashboard_user', RUTA_BASE . 'views/user/dashboardUser.php');
+
+// Rutas absolutas a controladores (controllers)
+define('ruta_controlador_user', RUTA_BASE . 'controllers/userController.php');
+define('ruta_controlador_check_code', RUTA_BASE . 'controllers/checkCode.php');
+define('ruta_controlador_send_code', RUTA_BASE . 'controllers/sendCode.php');
+define('ruta_controlador_update_pass', RUTA_BASE . 'controllers/updatePass.php');
+define('ruta_controlador_verificar_hash', RUTA_BASE . 'controllers/verificarHash.php');
+define('ruta_controlador_hash', RUTA_BASE . 'controllers/hash.php');
+
+// Rutas absolutas a modelos (models)
+define('ruta_modelo_user', RUTA_BASE . 'models/userModel.php');
+
+// Rutas absolutas a recursos públicos (public)
+define('ruta_css', RUTA_BASE . 'public/css/');
+define('ruta_images', RUTA_BASE . 'public/images/');
+
+
